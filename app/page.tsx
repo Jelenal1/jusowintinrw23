932867a3940a03c0390e2d53b8e6c17ec68a5f33
jelenal1 <jabbruh10@gmail.com>
2023-07-01 20:13:38 +0200

    Added basic cards to the index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,42 @@
 export default function Home() {
   return (
-    <>
-    </>
+    <div>
+      <h1 className="text-2xl text-center">Unsere Kanditat*innen</h1>
+      <div className=" m-1 bg-rose-200 rounded-lg py-2">
+        <div className="flex items-center gap-2">
+        <img src="/img/Valeria_muster-2.jpg" alt="Valeria Muster"  className="rounded-full aspect-square w-40 object-cover object-top"/>
+        <h2 className="text-sm">Ich würde meinen rechten Arm geben - den linken eher nicht so… ich bin Linkshändlerin</h2>
+        </div>
+        
+      </div>
+      <div className="flex items-center gap-2 m-1 bg-rose-200 rounded-lg py-2">
+        <div>
+        <img src="/img/M-Weidmann.jpg" alt="Valeria Muster"  className="rounded-full aspect-square w-40 object-cover object-top"/>
+        <h2 className="text-sm">Ich würde meinen rechten Arm geben - den linken eher nicht so… ich bin Linkshändlerin</h2>
+        </div>
+       
+      </div>
+      <div className="flex items-center gap-2 m-1 bg-rose-200 rounded-lg py-2">
+        <div>
+        <img src="/img/Rafael_Rüegg.jpg" alt="Valeria Muster"  className="rounded-full aspect-square w-40 object-cover object-top"/>
+        <h2 className="text-sm">Ich würde meinen rechten Arm geben - den linken eher nicht so… ich bin Linkshändlerin</h2>
+        </div>
+      
+      </div>
+      <div className="flex items-center gap-2 m-1 bg-rose-200 rounded-lg py-2">
+        <div>
+        <img src="/img/Pia_Voss.jpg" alt="Valeria Muster"  className="rounded-full aspect-square w-40 object-cover object-top"/>
+        <h2 className="text-sm">Ich würde meinen rechten Arm geben - den linken eher nicht so… ich bin Linkshändlerin</h2>
+        </div>
+       
+      </div>
+      <div className="flex items-center gap-2 m-1 bg-rose-200 rounded-lg py-2">
+        <div>
+        <img src="/img/J_Speer.jpg" alt="Valeria Muster"  className="rounded-full aspect-square w-40 object-cover object-top"/>
+        <h2 className="text-sm">Ich würde meinen rechten Arm geben - den linken eher nicht so… ich bin Linkshändlerin</h2>
+        </div>
+    
+      </div>
+    </div>
   )
 }
