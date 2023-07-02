14a4e76c4b653242a8f105847af2565e49cfb6bf
jelenal1 <jabbruh10@gmail.com>
2023-07-02 09:11:48 +0200

    I am working on the page for valeria

---
+++ app/valeriamuster/page.tsx
@@ -0,0 +1,14 @@
+export default function ValeriaMuster() {
+	return (
+		<div>
+			<h2>Valeria Muster</h2>
+		<div className='grid sm:grid-cols-2'>
+			<img src='/img/valeria_muster-2.jpg' className='object-cover object-top w-full' />
+		</div>
+		
+		<div>
+			
+		</div>
+		</div>
+	);
+}
