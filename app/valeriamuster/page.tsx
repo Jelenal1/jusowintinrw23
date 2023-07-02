091e59ee83a4b0b84c5b986550b74b4060e01b4f
jelenal1 <jabbruh10@gmail.com>
2023-07-02 13:49:31 +0200

    style: responsive looks now good

--- app/valeriamuster/page.tsx
+++ app/valeriamuster/page.tsx
@@ -1,13 +1,19 @@
 export default function ValeriaMuster() {
 	return (
 		<div>
-			<h2>Valeria Muster</h2>
-		<div className='grid sm:grid-cols-2'>
-			<img src='/img/valeria_muster-2.jpg' className='object-cover object-top w-full' />
-		</div>
-		
-		<div>
 			
+		<div className='grid sm:grid-cols-2 p-2 drop-shadow-sm'>
+			<img src='/img/valeria_muster-2.jpg' className='object-cover object-top w-full sm:aspect-square max-h-[800px]' />
+			<div className="object-cover object-top w-full flex flex-col justify-center max-h-[800px] p-2 border">
+				<div className="ml-2 mb-2">
+				<h2 className="lg:text-xl text-lg h-fit">Valeria Muster</h2>
+				<span className="text-xs">Sie/Ihr</span>
+				</div>
+			<h2 className="lg:text-2xl text-sm sm:ml-5">«Ich würde meinen rechten Arm geben - den linken eher nicht so… ich bin Linkshändlerin»</h2>
+			</div>
+		</div>
+		<div>
+		
 		</div>
 		</div>
 	);
