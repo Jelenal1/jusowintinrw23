e8043ca68f43adda445b5fda45205103cf02a93b
jelenal1 <jabbruh10@gmail.com>
2023-07-02 14:12:50 +0200

    added more content

--- app/valeriamuster/page.tsx
+++ app/valeriamuster/page.tsx
@@ -1,8 +1,8 @@
 export default function ValeriaMuster() {
 	return (
-		<div>
+		<div className="p-2 grid sm:grid-cols-2 drop-shadow-sm">
 			
-		<div className='grid sm:grid-cols-2 p-2 drop-shadow-sm'>
+
 			<img src='/img/valeria_muster-2.jpg' className='object-cover object-top w-full sm:aspect-square max-h-[800px]' />
 			<div className="object-cover object-top w-full flex flex-col justify-center max-h-[800px] p-2 border">
 				<div className="ml-2 mb-2">
@@ -10,11 +10,16 @@ export default function ValeriaMuster() {
 				<span className="text-xs">Sie/Ihr</span>
 				</div>
 			<h2 className="lg:text-2xl text-sm sm:ml-5">«Ich würde meinen rechten Arm geben - den linken eher nicht so… ich bin Linkshändlerin»</h2>
-			</div>
+
 		</div>
-		<div>
+		<div className="my-2 p-1 flex flex-col border">
+			<h2 className="text-xl mx-auto mb-5">Mein Leben</h2>
+		<p className="text-start text-lg">
+		Ich arbeite als Campaignerin bei der SP Zürich und über den Sommer als Blasintrumentenreparateurin (Ja crazy ich weiss, diesen Beruf gibt es wirklich… genau wie meinen Nachnamen auch  ). <br />
+		Meine freien Minuten fülle ich mit Musik machen und hören, sowie viel politischem Aktivismus bei der JUSO (oder auch der SP, wenn Mensch das Aktivismus nennen kann).
+		</p>
+		</div>
 		
-		</div>
 		</div>
 	);
 }
