968f2d123813ef379e15db2c5aed6109dbef825a
jelenal1 <jabbruh10@gmail.com>
2023-07-02 19:03:49 +0200

    finished main content of valerias site

--- app/valeriamuster/page.tsx
+++ app/valeriamuster/page.tsx
@@ -1,25 +1,56 @@
 export default function ValeriaMuster() {
+	const style = {
+		main: "p-2 grid",
+		quotecard: 'grid sm:grid-cols-2 drop-shadow-sm',
+		image: 'object-cover object-top w-full aspect-square max-h-[800px]',
+		quote: 'object-cover object-top w-full flex flex-col justify-center max-h-[800px] p-2 border',
+		paragraphwrapper: 'p-2 flex border flex-col mt-5',
+		paragraphtitel: 'text-2xl font-bold mb-5',
+		paragraph: 'sm:text-lg'
+
+	}
+
 	return (
-		<div className="p-2 grid sm:grid-cols-2 drop-shadow-sm">
-			
-
-			<img src='/img/valeria_muster-2.jpg' className='object-cover object-top w-full sm:aspect-square max-h-[800px]' />
-			<div className="object-cover object-top w-full flex flex-col justify-center max-h-[800px] p-2 border">
-				<div className="ml-2 mb-2">
-				<h2 className="lg:text-xl text-lg h-fit">Valeria Muster</h2>
-				<span className="text-xs">Sie/Ihr</span>
+		<div className={style.main}>
+			<div className={style.quotecard}>
+			<img
+				src='/img/valeria_muster-2.jpg'
+				className={style.image}
+			/>
+			<div className={style.quote}>
+				<div className='ml-2 mb-2'>
+					<h2 className='lg:text-xl text-lg h-fit'>Valeria Muster</h2>
+					<span className='text-xs'>Sie/Ihr</span>
 				</div>
-			<h2 className="lg:text-2xl text-sm sm:ml-5">«Ich würde meinen rechten Arm geben - den linken eher nicht so… ich bin Linkshändlerin»</h2>
+				<h2 className='lg:text-2xl text-sm sm:ml-5'>
+					«Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
+					bin Linkshändlerin»
+				</h2>
+			</div>
+			</div>
+			<div className={style.paragraphwrapper}>
+				<h2 className={style.paragraphtitel}>Mein Leben</h2>
+				<p className={style.paragraph}>
+					Ich arbeite als Campaignerin bei der SP Zürich und über den Sommer als
+					Blasintrumentenreparateurin (Ja crazy ich weiss, diesen Beruf gibt es
+					wirklich… genau wie meinen Nachnamen auch ). <br />
+					Meine freien Minuten fülle ich mit Musik machen und hören, sowie viel
+					politischem Aktivismus bei der JUSO (oder auch der SP, wenn Mensch das
+					Aktivismus nennen kann).
+				</p>
+			</div>
 
-		</div>
-		<div className="my-2 p-1 flex flex-col border">
-			<h2 className="text-xl mx-auto mb-5">Mein Leben</h2>
-		<p className="text-start text-lg">
-		Ich arbeite als Campaignerin bei der SP Zürich und über den Sommer als Blasintrumentenreparateurin (Ja crazy ich weiss, diesen Beruf gibt es wirklich… genau wie meinen Nachnamen auch  ). <br />
-		Meine freien Minuten fülle ich mit Musik machen und hören, sowie viel politischem Aktivismus bei der JUSO (oder auch der SP, wenn Mensch das Aktivismus nennen kann).
-		</p>
-		</div>
-		
+			<div className={style.paragraphwrapper}>
+				<h2 className={style.paragraphtitel}>Warum kandidiere ich</h2>
+				<p className={style.paragraph}>
+					1. Capitalism sucks! (Kapitalismus nervt!) <br />
+					2. Punkt Nummer 1 sollte reichen… <br />
+					3. …Falls nicht…. Wir leben in einer Zeit in welcher Genderstern und
+					Inklusion als «Woke-Wahnsinn» bezeichnet werden. Der rasante Klimawandel noch immer nicht von allen als Krise
+					wahrgenommen wird und es selbstverständlich ist, all diese Krisen auf
+					dem Buckel der 99 Prozent auszutragen.
+				</p>
+			</div>
 		</div>
 	);
 }
