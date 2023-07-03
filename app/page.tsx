a0e8367ac043dc61c43c6f62f8abe053d0f4ddc2
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-07-03 09:03:56 +0200

    style: restyled the landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -10,81 +10,81 @@ export default function Home() {
 		<>
 			<h1 className='text-2xl text-center'>Unsere Kanditat*innen</h1>
 			<div className='flex flex-col items-center lg:grid lg:grid-cols-2'>
-        <Link href="/valeriamuster">
-        <div className={style.card}>
-					<div className={style.cardimagewarpper}>
-						<img
-							src='/img/Valeria_muster-2.jpg'
-							alt='Valeria Muster'
-							className='rounded-full aspect-square w-40 object-cover object-top'
-						/>
-						<h2 className='text-sm'>
-							Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
-							bin Linkshändlerin
-						</h2>
+				<Link href="/valeriamuster">
+					<div className={style.card}>
+						<div className={style.cardimagewarpper}>
+							<img
+								src='/img/Valeria_muster-2.jpg'
+								alt='Valeria Muster'
+								className='rounded-full aspect-square w-32 sm:w-44 object-cover object-top'
+							/>
+							<h2 className='text-sm'>
+								Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
+								bin Linkshändlerin
+							</h2>
+						</div>
 					</div>
-				</div>
-        </Link>
-			<Link href="/michiweidmann">
-				<div className={style.card}>
-					<div className={style.cardimagewarpper}>
-						<img
-							src='/img/M-Weidmann.jpg'
-							alt='Valeria Muster'
-							className='rounded-full aspect-square w-40 object-cover object-top'
-						/>
-						<h2 className='text-sm'>
-							Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
-							bin Linkshändlerin
-						</h2>
+				</Link>
+				<Link href="/michiweidmann">
+					<div className={style.card}>
+						<div className={style.cardimagewarpper}>
+							<img
+								src='/img/M-Weidmann.jpg'
+								alt='Valeria Muster'
+								className='rounded-full aspect-square w-32 sm:w-44 object-cover object-top'
+							/>
+							<h2 className='text-sm'>
+								Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
+								bin Linkshändlerin
+							</h2>
+						</div>
 					</div>
-				</div>
-        </Link>
-        <Link href="/rafaelrueegg">
-				<div className={style.card}>
-					<div className={style.cardimagewarpper}>
-						<img
-							src='/img/Rafael_Rüegg.jpg'
-							alt='Valeria Muster'
-							className='rounded-full aspect-square w-40 object-cover object-top'
-						/>
-						<h2 className='text-sm'>
-							Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
-							bin Linkshändlerin
-						</h2>
+				</Link>
+				<Link href="/rafaelrueegg">
+					<div className={style.card}>
+						<div className={style.cardimagewarpper}>
+							<img
+								src='/img/Rafael_Rüegg.jpg'
+								alt='Valeria Muster'
+								className='rounded-full aspect-square w-32 sm:w-44 object-cover object-top'
+							/>
+							<h2 className='text-sm'>
+								Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
+								bin Linkshändlerin
+							</h2>
+						</div>
 					</div>
-				</div>
-        </Link>
-        <Link href="/piavoss">
-				<div className={style.card}>
-					<div className={style.cardimagewarpper}>
-						<img
-							src='/img/Pia_Voss.jpg'
-							alt='Valeria Muster'
-							className='rounded-full aspect-square w-40 object-cover object-top'
-						/>
-						<h2 className='text-sm'>
-							Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
-							bin Linkshändlerin
-						</h2>
+				</Link>
+				<Link href="/piavoss">
+					<div className={style.card}>
+						<div className={style.cardimagewarpper}>
+							<img
+								src='/img/Pia_Voss.jpg'
+								alt='Valeria Muster'
+								className='rounded-full aspect-square w-32 sm:w-44 object-cover object-top'
+							/>
+							<h2 className='text-sm'>
+								Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
+								bin Linkshändlerin
+							</h2>
+						</div>
 					</div>
-				</div>
-        </Link>
-        <Link href="/jelenaspeer">
-				<div className={style.card}>
-					<div className={style.cardimagewarpper}>
-						<img
-							src='/img/J_Speer.jpg'
-							alt='Valeria Muster'
-							className='rounded-full aspect-square w-40 object-cover object-top'
-						/>
-						<h2 className='text-sm'>
-							Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
-							bin Linkshändlerin
-						</h2>
+				</Link>
+				<Link href="/jelenaspeer">
+					<div className={style.card}>
+						<div className={style.cardimagewarpper}>
+							<img
+								src='/img/J_Speer.jpg'
+								alt='Valeria Muster'
+								className='rounded-full aspect-square w-32 sm:w-44 object-cover object-top'
+							/>
+							<h2 className='text-sm'>
+								Ich würde meinen rechten Arm geben - den linken eher nicht so… ich
+								bin Linkshändlerin
+							</h2>
+						</div>
 					</div>
-				</div>
-        </Link>
+				</Link>
 			</div>
 		</>
 	);
