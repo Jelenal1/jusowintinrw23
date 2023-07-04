7dd6e9d7ef9fb2de47d614df61ebd1795838ba29
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-07-04 23:00:12 +0200

    I added all the links that were missing

--- app/Navbar.jsx
+++ app/Navbar.jsx
@@ -3,29 +3,36 @@
 import { FaBars } from "react-icons/fa";
 import Sidenav from "./Sidenav";
 import { useState } from "react";
+import Link from "next/link";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="bg-rose-500 p-4 flex">
-      <div
-        className="flex items-center text-2xl gap-2 p-1 select-none"
-      >
-        <img
-          src="/img/jusologonoir.svg"
-          className="h-14"
-          alt="jusologo noir"
-        />
-        JUSO Winterthur
-      </div>
+      <Link href="/">
+        <div
+          className="flex items-center text-2xl gap-2 p-1 select-none"
+        >
+          <img
+            src="/img/jusologonoir.svg"
+            className="h-14"
+            alt="jusologo noir"
+          />
+          JUSO Winterthur
+        </div>
+      </Link>
+
       <div className="flex w-fit items-center p-1 ml-auto mr-2 hover: cursor-pointer">
         <FaBars
           className={"absolute right-8 h-10 w-10 aspect-square"}
           onClick={() => setOpen(true)}
         />
       </div>
-      <Sidenav open={open} setClosed={() => setOpen(false)} />
+      <Link href="/news">
+        <Sidenav open={open} setClosed={() => setOpen(false)} />
+      </Link>
+
     </div>
   );
 }
