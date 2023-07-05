fb6288ecb1ea9e0133731a0c813320b6c198ecc3
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-07-05 15:45:06 +0200

    I put the blogposts in an own component

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,46 +1,19 @@
-'use client'
-import { collection, query, where, onSnapshot, doc } from "firebase/firestore";
-
-import { db } from "./firebase.js";
-import { useState } from "react";
-import DisplayImage from "./DisplayImage";
+import BlogPosts from './BlogPosts';
 
 interface Post {
-    id: string;
-    date: string;
-    title: string;
-    content: string;
-    imagename: string;
+  id: string;
+  date: string;
+  title: string;
+  content: string;
+  imagename: string;
 }
 
-function BlogPosts() {
-    const [blogposts, setBlogposts] = useState<Post[]>([]);
-
-    const q = query(collection(db, "blogposts"));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        setBlogposts(
-            querySnapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title, date: doc.data().date, content: doc.data().content, imagename: doc.data().imagename }))
-        );
-    });
-
-    return (
-        <div className="p-2">
-            {blogposts.map((post) => {
-                return (
-                    <div
-                        className="flex flex-col border mx-auto text-center my-2 lg:w-3/4 "
-                        key={post.id}
-                    >
-                        <DisplayImage imagename={post.imagename} />
-                        <h1 className="text-2xl">{post.title}</h1>
-                        <h2 className="text-lg mb-4">{post.date}</h2>
-                        {post.content.split("\n").map((text, index) => {
-                            return <p className="text-lg" key={index}>{text}</p>;
-                        })}
-                    </div>
-                );
-            })}
-        </div>
-    );
+function RenderBlogposts() {
+  return (
+    <>
+      <h1>Blog</h1>
+      <BlogPosts />
+    </>
+  );
 }
-export default BlogPosts;
+export default RenderBlogposts;
