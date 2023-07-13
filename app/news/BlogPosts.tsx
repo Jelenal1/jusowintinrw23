40be5b986cf3f0cf52c2d92f4b556c10aa67df4a
jelenal1 <jabbruh10@gmail.com>
2023-07-13 21:44:29 +0200

    The upload form works now

--- app/news/BlogPosts.tsx
+++ app/news/BlogPosts.tsx
@@ -1,46 +1,60 @@
-'use client'
+"use client";
 import { collection, query, where, onSnapshot, doc } from "firebase/firestore";
 
 import { db } from "./firebase.js";
 import { useState } from "react";
-import DisplayImage from "./DisplayImage";
 
 interface Post {
-    id: string;
-    date: string;
-    title: string;
-    content: string;
-    imagename: string;
+	id: string;
+	date: string;
+	title: string;
+	content: string;
+	imageurl: string;
 }
 
 function BlogPosts() {
-    const [blogposts, setBlogposts] = useState<Post[]>([]);
+	const [blogposts, setBlogposts] = useState<Post[]>([]);
 
-    const q = query(collection(db, "blogposts"));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        setBlogposts(
-            querySnapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title, date: doc.data().date, content: doc.data().content, imagename: doc.data().imagename }))
-        );
-    });
+	const q = query(collection(db, "blogposts"));
+	const unsubscribe = onSnapshot(q, (querySnapshot) => {
+		setBlogposts(
+			querySnapshot.docs.map((doc) => ({
+				id: doc.id,
+				title: doc.data().title,
+				date: doc.data().date,
+				content: doc.data().content,
+				imageurl: doc.data().imageurl,
+			}))
+		);
+	});
 
-    return (
-        <div className="p-2">
-            {blogposts.map((post) => {
-                return (
-                    <div
-                        className="flex flex-col border mx-auto text-center my-2 lg:w-3/4 "
-                        key={post.id}
-                    >
-                        <DisplayImage imagename={post.imagename} />
-                        <h1 className="text-2xl">{post.title}</h1>
-                        <h2 className="text-lg mb-4">{post.date}</h2>
-                        {post.content.split("\n").map((text, index) => {
-                            return <p className="text-lg" key={index}>{text}</p>;
-                        })}
-                    </div>
-                );
-            })}
-        </div>
-    );
+	return (
+		<div className='p-2'>
+			{blogposts.map((post) => {
+				return (
+					<div
+						className='flex flex-col border mx-auto text-center my-2 lg:w-3/4 '
+						key={post.id}
+					>
+						<div className='h-96 aspect-square'>
+							<img
+								src={post.imageurl}
+								className='object-cover w-full h-full'
+							/>
+						</div>
+						<h1 className='text-2xl'>{post.title}</h1>
+						<h2 className='text-lg mb-4'>{post.date}</h2>
+						{post.content.split("\n").map((text, index) => {
+							return (
+								<p className='text-lg' key={index}>
+									{text}
+								</p>
+							);
+						})}
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 export default BlogPosts;
