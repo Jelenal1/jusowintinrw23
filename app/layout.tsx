1361abd44aed91b9ba3b4e342444777bbb0dc490
jelenal1 <jabbruh10@gmail.com>
2023-09-07 18:17:37 +0200

    I try to add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,7 +22,10 @@ export default function RootLayout({
         {
           <div className='flex flex-col min-h-screen'>
             <Navbar />
-            <main className='flex-1'>{children}</main>
+            <main className='flex-1'>
+              {children}
+            <Analytics />
+            </main>
             <Footer />
           </div>
         }
